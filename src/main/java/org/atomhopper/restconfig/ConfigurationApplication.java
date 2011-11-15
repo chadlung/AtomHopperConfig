e223b45a246a173ef34792bfc1e0cc957a8489b4
Chad Lung <chad.lung@gmail.com>
2011-11-15 14:57:29 -0600

    - moved to different package

--- src/main/java/org/atomhopper/restconfig/ConfigurationApplication.java
+++ src/main/java/org/atomhopper/restconfig/ConfigurationApplication.java
@@ -1,13 +1,13 @@
-package org.atomhopper.restconfig.application;
-
+package org.atomhopper.restconfig;
 
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import javax.ws.rs.ApplicationPath;
+
 
 @ApplicationPath("/")
 public class ConfigurationApplication extends PackagesResourceConfig {
 
     public ConfigurationApplication() {
-        super("org.atomhopper.resources.impl");
+        super("org.atomhopper.restconfig.resources.impl");
     }
 }
